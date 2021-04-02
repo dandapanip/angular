8c05fa530af01b4285f55e79c696c48ee71c47fa
Dandapani <dandapani.paneerselvam@ameexusa.com>
2021-04-02 10:59:38 +0530

    Login and Register

--- myBookStore/src/app/login/login.component.ts
+++ myBookStore/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpDataService } from '../http-data.service';
 
 @Component({
   selector: 'mbs-login',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  // formData: {userName: string, password: string} = {userName: '', password: ''}
+  userdata: any
+  @ViewChild('f', { static: false }) signupForm: NgForm;
+  user = {
+    user_Email: '',
+    user_password: ''
+  }
+  constructor(private http: HttpDataService) { }
 
   ngOnInit(): void {
+    this.http.get('/user').subscribe((data)=> {
+      this.userdata = data;
+      console.log(this.userdata);
+    })
   }
+  onSubmit() {
+    this.user.user_Email = this.signupForm.value.user_Email;
+    this.user.user_password = this.signupForm.value.user_password;
+    // console.log(this.user.username,this.user.password);
+    const validation = this.userdata.filter(element => (this.user.user_Email == element.user_Email && this.user.user_password == element.user_password))
+    console.log(validation);
+      if(validation == ""){
+        alert("Invalid Username or password");
+      } else {
+        alert("Successfull");
+      }
+      
+  }
+
+
 
 }
--- myBookStore/src/app/register/register.component.ts
+++ myBookStore/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpDataService } from '../http-data.service';
 
 @Component({
   selector: 'mbs-register',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  userdata: any
+  
+  @ViewChild('f', { static: false }) signupForm: NgForm;
+  user = {
+    firstname: '',
+    lastname: '',
+    user_Email: '',
+    user_password: ''
+  }
 
-  constructor() { }
+  constructor(private http: HttpDataService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.user.firstname = this.signupForm.value.firstname;
+    this.user.lastname = this.signupForm.value.lastname;
+    this.user.user_Email = this.signupForm.value.user_Email;
+    this.user.user_password = this.signupForm.value.user_password;
+    console.log(this.user.firstname,this.user.lastname,this.user.user_Email,this.user.user_password);
+    //  post details
+      this.http.post('/user',this.user).subscribe(()=> {
+            // get details
+    this.http.get('/user').subscribe((data)=> {
+      this.userdata = data
+      console.log("Check Data",this.userdata);
+    })
+    })
   }
 
 }
